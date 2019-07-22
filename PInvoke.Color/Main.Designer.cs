10c1f3a17439ffab4fbd4508cde1b16cfb4fb41d
Lucas juliano <lucas.juliano@live.com>
2019-07-22 10:38:53 -0300

    demo

--- PInvoke.Color/Main.Designer.cs
+++ PInvoke.Color/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PInvoke.Color
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(257, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Main Forms";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ PInvoke.Color/Main.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PInvoke.Color
+{
+    public partial class Main : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        readonly Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private async void Main_Load(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    var cursor = new Point();
+
+                    GetCursorPos(ref cursor);
+
+                    var c = GetColorAt(cursor);
+
+                    this.BackColor = c;
+
+                    Action<string> action = (color) => this.label1.Text = $"{color}";
+
+                    this.Invoke(action, new object[] { c.Name });
+                }
+            });
+        }
+
+        private System.Drawing.Color GetColorAt(
+            Point location)
+        {
+            using (var gdest = Graphics.FromImage(screenPixel))
+            {
+                using (var gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    var hSrcDC = gsrc.GetHdc();
+                    var hDC = gdest.GetHdc();
+                    var retval = BitBlt(
+                        hDC,
+                        0,
+                        0,
+                        1,
+                        1,
+                        hSrcDC,
+                        location.X,
+                        location.Y,
+                        (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
+    }
+}
